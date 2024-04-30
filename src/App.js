b4064816ad8e908bd06bd7d312fe9edfd5e1489e
Ahmed-wael2002 <Ahmedwael978@gmail.com>
2024-04-30 18:14:57 +0300

    Add env api url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // App.js
 
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const BASE_URL =
+  process.env.REACT_APP_BACKEND_BASE_URL || "http://dist-proj-api.mu-stafa.com";
 
 function App() {
   const [images, setImages] = useState([]);
-  const [operation, setOperation] = useState('BLUR');
+  const [operation, setOperation] = useState("BLUR");
   const [processedImageUrl, setProcessedImageUrl] = useState(null);
 
   const handleImageUpload = (event) => {
@@ -19,49 +22,71 @@ function App() {
 
   const processImages = () => {
     const formData = new FormData();
-    formData.append('image', images[0]);
-    formData.append('operation', operation);
+    formData.append("image", images[0]);
+    formData.append("operation", operation);
 
-    fetch('http://dist-proj-api.mu-stafa.com/api/v1/image_processing', {
-      method: 'POST',
+    fetch(`${BASE_URL}/api/v1/image_processing`, {
+      method: "POST",
       body: formData,
-      mode: 'no-cors'
+      mode: "cors",
     })
-    .then(response => {
-      if (response.ok) {
-        return response.blob(); // Convert response to Blob object
-      } else {
-        throw new Error('Error processing images');
-      }
-    })
-    .then(blob => {
-      const imageUrl = URL.createObjectURL(blob); // Create object URL for the Blob
-      setProcessedImageUrl(imageUrl); // Set processed image URL
-    })
-    .catch(error => {
-      console.error('Error processing images:', error);
-    });
+      .then((response) => {
+        if (response.ok) {
+          return response.blob(); // Convert response to Blob object
+        } else {
+          throw new Error("Error processing images");
+        }
+      })
+      .then((blob) => {
+        const imageUrl = URL.createObjectURL(blob); // Create object URL for the Blob
+        setProcessedImageUrl(imageUrl); // Set processed image URL
+      })
+      .catch((error) => {
+        console.error("Error processing images:", error);
+      });
   };
 
   return (
     <div>
       <div className="container">
         <h1>Image Processing App</h1>
-        <input type="file" accept="image/*" onChange={handleImageUpload} className="file-input" />
-        <label htmlFor="operation-select" className="select-label">Choose Operation:</label>
-        <select id="operation-select" value={operation} onChange={handleOperationChange} className="select-operation">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          className="file-input"
+        />
+        <label htmlFor="operation-select" className="select-label">
+          Choose Operation:
+        </label>
+        <select
+          id="operation-select"
+          value={operation}
+          onChange={handleOperationChange}
+          className="select-operation"
+        >
           <option value="BLUR">BLUR</option>
           <option value="EMBOSS">EMBOSS</option>
           <option value="SHARPEN">SHARPEN</option>
         </select>
-        <button onClick={processImages} className="button">Process Images</button>
+        <button onClick={processImages} className="button">
+          Process Images
+        </button>
       </div>
       {processedImageUrl && (
         <div className="processed-image-container">
-          <h2 style={{
-            textAlign: 'center'
-          }}>Processed Image</h2>
-          <img src={processedImageUrl} alt="Processed" className="processed-image" />
+          <h2
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Processed Image
+          </h2>
+          <img
+            src={processedImageUrl}
+            alt="Processed"
+            className="processed-image"
+          />
         </div>
       )}
     </div>
